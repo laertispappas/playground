f52d0a8ffad590347466eb38eb182ee87bfa999d
Laerti Papa <laerti.papa@xing.com>
2018-10-05 10:55:15 +0200

    Inject http client and adds news api key

--- angular/news-demo/src/app/news-api.service.ts
+++ angular/news-demo/src/app/news-api.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class NewsApiService {
+  static readonly API_KEY = "94f93287be014d6db322a1449ef75978";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 }
