1e93da739dc7db95ee0399df14e44faa1d4262fd
Laerti Papa <laerti.papa@xing.com>
2018-10-05 11:05:50 +0200

    Loads articles on component level

--- angular/news-demo/src/app/app.component.ts
+++ angular/news-demo/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { NewsApiService } from "./news-api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'news-demo';
+export class AppComponent implements OnInit {
+  title = "news-demo";
+  articles: any[];
+  sources: any[];
+
+  constructor(private newsapi: NewsApiService) {}
+
+  ngOnInit() {
+    this.newsapi
+      .getArticles()
+      .subscribe(data => (this.articles = data["articles"]));
+
+    this.newsapi
+      .getSources()
+      .subscribe(data => (this.sources = data["sources"]));
+  }
 }
