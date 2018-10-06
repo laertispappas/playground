6a73e8fa5e9402f2ed1c226f2059225dae30ab98
Laerti Papa <laerti.papa@xing.com>
2018-10-06 17:55:36 +0200

    Adds other feature module store (will not use ngrx entity)

--- angular/ngrx-best-practice/src/app/root-store/root-store.module.ts
+++ angular/ngrx-best-practice/src/app/root-store/root-store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFeatureStoreModule } from './my-feature-store/my-feature-store.module';
+import { OtherFeatureStoreModule } from './other-feature-store/other-feature-store.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MyFeatureStoreModule
+    MyFeatureStoreModule,
+    OtherFeatureStoreModule
   ],
   declarations: []
 })
