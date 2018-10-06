6d08994d371468f40972b92b090d2a74877f735c
Laerti Papa <laerti.papa@xing.com>
2018-10-06 18:46:06 +0200

    Adds sample usage example for selecting and dispatching actions

--- angular/ngrx-best-practice/src/app/app.component.ts
+++ angular/ngrx-best-practice/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { MyModel } from './models';
+
+import {
+  RootStoreState,
+  MyFeatureStoreActions,
+  MyFeatureStoreSelectors
+} from './root-store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myFeatureItems$: Observable<MyModel[]>;
+  error$: Observable<string>;
+  isLoading$: Observable<boolean>;
+
   title = 'ngrx-best-practice';
+
+  constructor(private store$: Store<RootStoreState.RootState>) {}
+
+  ngOnInit() {
+    this.myFeatureItems$ = this.store$.select(
+      MyFeatureStoreSelectors.selectAllMyFeatureItems
+    );
+
+    this.error$ = this.store$.select(
+      MyFeatureStoreSelectors.selectMyFeatureError
+    );
+
+    this.isLoading$ = this.store$.select(
+      MyFeatureStoreSelectors.selectMyFeatureIsLoading
+    );
+
+    this.store$.dispatch(new MyFeatureStoreActions.LoadRequestAction());
+  }
 }
