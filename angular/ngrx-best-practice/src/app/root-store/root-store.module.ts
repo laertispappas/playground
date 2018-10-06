af329e8a840230f9b25fa85d6edf5d2962179c5f
Laerti Papa <laerti.papa@xing.com>
2018-10-06 17:01:41 +0200

    Adds my feature store skeleton

--- angular/ngrx-best-practice/src/app/root-store/root-store.module.ts
+++ angular/ngrx-best-practice/src/app/root-store/root-store.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyFeatureStoreModule } from './my-feature-store/my-feature-store.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MyFeatureStoreModule
   ],
   declarations: []
 })
