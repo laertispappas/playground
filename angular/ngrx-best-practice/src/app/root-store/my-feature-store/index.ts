88df0f928d507c4b494b0f734f63d05645de6660
Laerti Papa <laerti.papa@xing.com>
2018-10-06 18:20:45 +0200

    Re export -- name-space -- my feature store components

---
+++ angular/ngrx-best-practice/src/app/root-store/my-feature-store/index.ts
@@ -0,0 +1,7 @@
+import * as MyFeatureStoreActions from './actions';
+import * as MyFeatureStoreSelectors from './selectors';
+import * as MyFeatureStoreState from './state';
+
+export { MyFeatureStoreModule } from './my-feature-store.module';
+
+export { MyFeatureStoreActions, MyFeatureStoreSelectors, MyFeatureStoreState };
