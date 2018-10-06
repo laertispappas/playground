ec3731478d93cd93446961b4e78ba7e4f384ec73
Laerti Papa <laerti.papa@xing.com>
2018-10-06 18:20:16 +0200

    Import store and effects in my feature module

--- angular/ngrx-best-practice/src/app/root-store/my-feature-store/my-feature-store.module.ts
+++ angular/ngrx-best-practice/src/app/root-store/my-feature-store/my-feature-store.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { MyFeatureStoreEffects } from './effects';
+import { featureReducer } from './reducer';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: []
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('myFeature', featureReducer),
+    EffectsModule.forFeature([MyFeatureStoreEffects])
+  ],
+  providers: [MyFeatureStoreEffects]
 })
 export class MyFeatureStoreModule {}
