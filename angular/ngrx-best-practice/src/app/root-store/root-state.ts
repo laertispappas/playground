c07dcc02258e84376d949edc837b83981dba9bff
Laerti Papa <laerti.papa@xing.com>
2018-10-06 18:22:24 +0200

    Adds myFeatureStoreState to root state

--- angular/ngrx-best-practice/src/app/root-store/root-state.ts
+++ angular/ngrx-best-practice/src/app/root-store/root-state.ts
@@ -1,4 +1,8 @@
+import { MyFeatureStoreState } from './my-feature-store';
+
 /*
   Root state interface to hold all our application state.
 */
-export interface RootState {}
+export interface RootState {
+  myFeature: MyFeatureStoreState.State;
+}
