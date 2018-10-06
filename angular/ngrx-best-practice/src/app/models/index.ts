fe035bca7b3e6e52e0400acb13fbfd69f1c8e64b
Laerti Papa <laerti.papa@xing.com>
2018-10-06 17:41:20 +0200

    Abstract service for CRUD operations to a generic resource service

--- angular/ngrx-best-practice/src/app/models/index.ts
+++ angular/ngrx-best-practice/src/app/models/index.ts
@@ -1,2 +1,5 @@
 export * from './my_model';
 export * from './my_model_serializer';
+export * from './resource';
+export * from './serializer';
+export * from './query-options';
--- angular/ngrx-best-practice/src/app/services/data.service.ts
+++ angular/ngrx-best-practice/src/app/services/data.service.ts
@@ -1,40 +1,15 @@
+import { MyModel, MyModelSerializer } from '../models';
+import { ResourceService } from './resource.service';
 import { HttpClient } from '@angular/common/http';
-import { MyModelSerializer, MyModel } from '../models';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
-export class DataService {
-  private url = 'http://apiservice';
-  private endpoint = 'my_model';
-  private serializer = new MyModelSerializer();
-
-  constructor(protected httpClient: HttpClient) {}
-
-  public create(model: MyModel): Observable<MyModel> {
-    return this.httpClient
-      .post<MyModel>(`${this.url}/${this.endpoint}`, model)
-      .pipe(map(data => this.serializer.fromJson(data)));
-  }
-
-  public update(model: MyModel): Observable<MyModel> {
-    return this.httpClient
-      .put<MyModel>(`${this.url}/${this.endpoint}/${model.id}`, model)
-      .pipe(map(data => this.serializer.fromJson(data)));
-  }
-
-  read(id: number): Observable<MyModel> {
-    return this.httpClient
-      .get<MyModel>(`${this.url}/${this.endpoint}/${id}`)
-      .pipe(map(data => this.serializer.fromJson(data)));
-  }
-
-  list(): Observable<MyModel[]> {
-    return this.httpClient
-      .get<MyModel[]>(`${this.url}/${this.endpoint}`)
-      .pipe(map(data => data.map(item => this.serializer.fromJson(item))));
-  }
-
-  delete(id: number) {
-    return this.httpClient.delete(`${this.url}/${this.endpoint}/${id}`);
+// Use generic CRUD resource service for CRUD operations
+export class DataService extends ResourceService<MyModel> {
+  constructor(httpClient: HttpClient) {
+    super(
+      httpClient,
+      'http://apiservice',
+      'my_models',
+      new MyModelSerializer()
+    );
   }
 }
